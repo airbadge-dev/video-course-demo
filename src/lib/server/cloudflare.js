fe7835be96d6ff738d5ff9003170904ec73c207f
Josh Nussbaum <joshnuss@gmail.com>
2024-06-05 16:04:43 -0400

    Refactor cloudflare api call

--- src/lib/server/cloudflare.js
+++ src/lib/server/cloudflare.js
@@ -4,19 +4,17 @@ import {
   CLOUDFLARE_ACCESS_TOKEN
 } from '$env/static/private'
 
+const authorization = `Bearer ${CLOUDFLARE_ACCESS_TOKEN}`
+
 export async function signUrl(video) {
-  const response = await fetch(
-    `https://api.cloudflare.com/client/v4/accounts/${CLOUDFLARE_ACCOUNT_ID}/stream/${video.vid}/token`,
-    {
-      method: 'POST',
-      headers: {
-        authorization: `Bearer ${CLOUDFLARE_ACCESS_TOKEN}`
-      }
-    }
-  ).then((r) => r.json())
+  const url = `https://api.cloudflare.com/client/v4/accounts/${CLOUDFLARE_ACCOUNT_ID}/stream/${video.vid}/token`
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: { authorization }
+  }).then((r) => r.json())
 
   const token = response.result.token
-  const url = `https://${CLOUDFLARE_DOMAIN}/${token}/manifest/video.m3u8`
+  const signedUrl = `https://${CLOUDFLARE_DOMAIN}/${token}/manifest/video.m3u8`
 
-  return url
+  return signedUrl
 }
